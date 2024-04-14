dca3e0162f88bd0a4e168a547e41a026f6d28b05
shivaprasad-i <shivuindaragi2500@gmail.com>
2024-04-14 10:01:20 +0530

    Additional files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CityListingComponent } from './sgi/city-listing/city-listing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CityListingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
